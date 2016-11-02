f9acba34f0703a13c1a0d49b58074cf2e9e548a4
Yoann Laissus <yoann.laissus@gmail.com>
2016-11-02 22:18:54 +0100

    Puppet 4.

--- manifests/init.pp
+++ manifests/init.pp
@@ -53,7 +53,7 @@ class sshd(
   }
 
   if $manage_nagios {
-    sshd::nagios{$ports:
+    sshd::nagios{"$ports":
       check_hostname => $nagios_check_ssh_hostname,
       target_server_name => $nagios_target_server_name
     }
